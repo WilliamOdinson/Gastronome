2c3a580d305f7a00f0218174e76ea338a91e80e7
William Sun <109450999+WilliamOdinson@users.noreply.github.com>
2025-05-24 12:49:23 +0800

    chore: update import timing estimates and ignore celerybeat artifacts
    adjust estimated durations in init.sh based on windows hardware benchmarks
    add celerybeat-related files to .gitignore

--- init.sh
+++ init.sh
@@ -20,24 +20,23 @@ DATA_DIR="./database"
 # echo ">>> Creating superuser (optional)..."
 # python manage.py createsuperuser || true
 
-echo "===The following import may take about 280 minutes (for an 8GB M2 Macbook). ==="
-echo "===If your hardware is better, please adjust the batch size.==="
+echo "===The following import may take about 160 minutes (for a 12th Gen Intel i9, 64 GB RAM Windows Laptop). ==="
 echo "===You can comment out line 39 if you read the entire init.sh before executing.==="
 echo ">>> Press any key to continue..." && stty -echo -icanon && dd bs=1 count=1 2>/dev/null < /dev/tty && stty icanon echo
 
 echo ">>> Importing category data, 1,311 entires (~0.1 min)"
 python manage.py import_category "$DATA_DIR/yelp_academic_dataset_business.json"
 
-echo ">>> Importing business data, 150,346 entires (~2 min for batch = 1,000)"
+echo ">>> Importing business data, 150,346 entires (~5 min for batch = 1,000)"
 python manage.py import_business "$DATA_DIR/yelp_academic_dataset_business.json"
 
-echo ">>> Importing hour data, 150,346 entires (~2 min for batch = 5,000)"
+echo ">>> Importing hour data, 150,346 entires (~5 min for batch = 5,000)"
 python manage.py import_hour "$DATA_DIR/yelp_academic_dataset_business.json"
 
-echo ">>> Importing user data, 1,987,897 entires (~16 min for batch = 5,000)"
+echo ">>> Importing user data, 1,987,897 entires (~10 min for batch = 2,500)"
 python manage.py import_user "$DATA_DIR/yelp_academic_dataset_user.json"
 
-echo ">>> Importing user's email data, 1,987,897 entires (~25 min for batch = 10,000)"
+echo ">>> Importing user's email data, 1,987,897 entires (~30 min for batch = 10,000)"
 python manage.py import_email
 
 echo ">>> Importing user's password data, 1,987,897 entires (~1 min)"
@@ -45,7 +44,7 @@ echo ">>> [\!note]: Did you set the password in the .env file?"
 echo ">>> Press any key to continue..." && stty -echo -icanon && dd bs=1 count=1 2>/dev/null < /dev/tty && stty icanon echo
 python manage.py import_password
 
-echo ">>> Importing checkin data, 131,930 entires (~14 min for batch = 5,000)"
+echo ">>> Importing checkin data, 131,930 entires (~15 min for batch = 5,000)"
 python manage.py import_checkin "$DATA_DIR/yelp_academic_dataset_checkin.json"
 
 echo ">>> Importing photo data, 201,000 entires (~0.1 min for batch = 10,000)"
@@ -53,13 +52,13 @@ echo ">>> [\!note]: photo.json is in Yelp Photos dataset"
 echo ">>> Press any key to continue..." && stty -echo -icanon && dd bs=1 count=1 2>/dev/null < /dev/tty && stty icanon echo
 python manage.py import_photo "$DATA_DIR/photos.json"
 
-echo ">>> Importing reviews, 6,990,280 entires (~150 min for batch = 10,000)"
+echo ">>> Importing reviews, 6,990,280 entires (~40 min for batch = 5,000)"
 python manage.py import_review "$DATA_DIR/yelp_academic_dataset_review.json"
 
 echo ">>> Importing tips, 908,915 entires (~2 min for batch = 10,000)"
 python manage.py import_tip "$DATA_DIR/yelp_academic_dataset_tip.json"
 
-echo ">>> Importing auto_score scored by our judge, 6,990,280 entires (~60 min for batch = 2,000)"
+echo ">>> Importing auto_score scored by our judge, 6,990,280 entires (~50 min for batch = 2,000)"
 python manage.py import_autoscore "$DATA_DIR/review_predictions.json"
 
 echo ">>> All done. Run the server with:"
